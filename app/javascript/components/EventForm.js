96bcd75860e7c02fcdc76f8dcc40538cbf77a35a
Efstathios Stivaros <efstathiosstivaros@gmail.com>
2019-12-22 23:05:04 +0000

    :construction: Implement functions for validation on new event form

--- app/javascript/components/EventForm.js
+++ app/javascript/components/EventForm.js
@@ -1,50 +1,127 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class EventForm extends React.Component {
   constructor(props) {
     super(props);
 
+    this.state = {
+      event: props.event,
+      errors: {},
+    };
+
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleInputChange = this.handleInputChange.bind(this);
   }
 
   handleSubmit(e) {
     e.preventDefault();
-    console.log('Submitted');
+    const { event } = this.state;
+    const errors = this.validateEvent(event);
+    if (!this.isEmptyObject(errors)) {
+      this.setState({ errors });
+    } else {
+      console.log(event);
+    }
+  }
+
+  validateEvent(event) {
+    const errors = {};
+
+    if (event.event_type === '') {
+      errors.event_type = 'You must enter an event_type';
+    }
+
+    if (event.event_date === '') {
+      errors.event_date = 'You must enter a valid date';
+    }
+
+    if (event.title === '') {
+      errors.title = 'You must enter a title';
+    }
+
+    if (event.speaker === '') {
+      errors.speaker = 'You must enter at least one speaker';
+    }
+
+    if (event.host === '') {
+      errors.host = 'You must enter at least one host';
+    }
+
+    console.log(event);
+    return errors;
+  }
+
+  isEmptyObject(obj) {
+    return Object.keys(obj).length === 0;
+  }
+
+  handleInputChange(event) {
+    const { target } = event;
+    const { name } = target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+
+    this.setState(prevState => ({
+      event: {
+        ...prevState.event,
+        [name]: value,
+      },
+    }));
+  }
+
+  renderErrors() {
+    const { errors } = this.state;
+
+    if (this.isEmptyObject(errors)) {
+      return null;
+    }
+
+    return (
+      <div className="errors">
+        <h3>The following errors prohibited the event from being saved:</h3>
+        <ul>
+          {Object.values(errors).map(error => (
+            <li key={error}>{error}</li>
+          ))}
+        </ul>
+      </div>
+    );
   }
 
   render() {
     return (
       <div>
         <h2>NewEvent</h2>
+        {this.renderErrors()}
         <form className="eventForm" onSubmit={this.handleSubmit}>
           <div>
             <label htmlFor="event_type">
               <strong>Type:</strong>
-              <input type="text" id="event_type" name="event_type" />
+              <input type="text" id="event_type" name="event_type" onChange={this.handleInputChange} />
             </label>
           </div>
           <div>
             <label htmlFor="event_date">
               <strong>Date:</strong>
-              <input type="text" id="event_date" name="event_date" />
+              <input type="text" id="event_date" name="event_date" onChange={this.handleInputChange} />
             </label>
           </div>
           <div>
             <label htmlFor="title">
               <strong>Title:</strong>
-              <textarea name="title" id="title" cols="30" rows="10" />
+              <textarea name="title" id="title" cols="30" rows="10" onChange={this.handleInputChange} />
             </label>
           </div>
           <div>
             <label htmlFor="speaker">
               <strong>Speakers:</strong>
-              <input type="text" id="speaker" name="title" />
+              <input type="text" id="speaker" name="title" onChange={this.handleInputChange} />
             </label>
           </div>
           <div>
             <label htmlFor="published">
               <strong>Publish:</strong>
-              <input type="text" id="published" name="published" />
+              <input type="text" id="published" name="published" onChange={this.handleInputChange} />
             </label>
           </div>
           <div className="form-actions">
@@ -55,5 +132,20 @@ class EventForm extends React.Component {
     );
   }
 }
+
+EventForm.propTypes = {
+  event: PropTypes.shape(),
+};
+
+EventForm.defaultProps = {
+  event: {
+    event_type: '',
+    event_date: '',
+    title: '',
+    speaker: '',
+    host: '',
+    published: false,
+  },
+};
 
 export default EventForm;
